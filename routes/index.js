c1555204d7c852baf00c47cb91f68697a0de7346
Ana Martin <anamartinleg@gmail.com>
2015-04-30 23:11:11 +0200

    creditos

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,15 @@ var quizController = require('../controllers/quiz_controller');
  router.get('/', function(req, res) {
    res.render('index', { title: 'Quiz' });
  });
+
+  /* GET authors page. */
+ router.get('/author', function(req, res) {
+   res.render('author');
+ });
  
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer',   quizController.answer);
+
 
 
  module.exports = router;
