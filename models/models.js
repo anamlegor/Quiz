60b69e4506e3339f0638d64d076534d56e201fa2
Ana Martin <anamartinleg@gmail.com>
2015-05-18 17:51:55 +0200

    Despliegue en Heroku de la búsqueda

--- models/models.js
+++ models/models.js
@@ -3,7 +3,7 @@ var path = require('path');
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
 // SQLite   DATABASE_URL = sqlite://:@:/
 
-//Linea comentada
+
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name  = (url[6]||null);
 var user     = (url[2]||null);
