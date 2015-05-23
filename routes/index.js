fdf9f90eb09c8fe14bb587c758fef135be46f755
Ana Martin <anamartinleg@gmail.com>
2015-05-23 22:19:54 +0200

    Registro de usuarios y gestion de cuentas

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
+var userController = require('../controllers/user_controller');
 
  /* GET home page. */
  // Página de entrada (home page)
@@ -11,13 +12,24 @@ var sessionController = require('../controllers/session_controller');
    res.render('index', { title: 'Quiz', errors: []});
  });
 
+// Autoload de comandos con ids
 router.param('quizId', quizController.load); // autoload :quizId
 router.param('commentId', commentController.load);  // autoload :commentId
+router.param('userId', userController.load);  // autoload :userId
+
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
 router.post('/login', sessionController.create);  // crear sesión
 router.get('/logout', sessionController.destroy); // destruir sesión
+
+// Definición de rutas de cuenta
+router.get('/user',  userController.new);     // formulario sign un
+router.post('/user',  userController.create);     // registrar usuario
+router.get('/user/:userId(\\d+)/edit',  sessionController.loginRequired, userController.edit);     // editar información de cuenta
+router.put('/user/:userId(\\d+)',  sessionController.loginRequired, userController.update);     // actualizar información de cuenta
+router.delete('/user/:userId(\\d+)',  sessionController.loginRequired, userController.destroy);     // borrar cuenta
+
 
   /* GET authors page. */
  router.get('/author', function(req, res) {
